184c18566f110ad1b33de950ca6b825bca8d239a
Elijah Orlov <elijah.n.orlov@yandex.ru>
2024-12-25 01:13:53 +0300

    fix tests

--- failed_test_caesar.py
+++ failed_test_caesar.py
@@ -5,15 +5,14 @@ def failed_test_caesar():
     """
     Не валидная проверка на корректную зашифровку и последующую расшифровку текста со сдвигом 15
     """
-    decrypted_text = "ilunination"
+    decrypted_text = "ilumination"
     encrypted_text = "vyhzvangvba"  # текст со сдвигом 13
     caesar = Caesar(15)
     check_encrypted_text = caesar.encode(decrypted_text)
     check_decrypted_text = caesar.decode(check_encrypted_text)
     assert check_encrypted_text == encrypted_text, f"Ошибка: зашифрованный текст: '{check_encrypted_text}' != '{encrypted_text}'"
     assert check_decrypted_text == decrypted_text, f"Ошибка: расшифрованный текст: '{check_decrypted_text}' != '{decrypted_text}'"
-
-    print("Все тесты пройдены успешно")
+    print("All tests PASSED")
 
 
 failed_test_caesar()
--- test_caesar.py
+++ test_caesar.py
@@ -5,15 +5,14 @@ def correct_test_caesar():
     """
     Проверка на корректную зашифровку и последующую расшифровку текста со сдвигом 15
     """
-    decrypted_text = "ilunination"
+    decrypted_text = "ilumination"
     encrypted_text = "xajbxcpixdc"
     caesar = Caesar(15)
     check_encrypted_text = caesar.encode(decrypted_text)
     check_decrypted_text = caesar.decode(check_encrypted_text)
     assert check_encrypted_text == encrypted_text, f"Ошибка: зашифрованный текст: '{check_encrypted_text}' != '{encrypted_text}'"
     assert check_decrypted_text == decrypted_text, f"Ошибка: расшифрованный текст: '{check_decrypted_text}' != '{decrypted_text}'"
-
-    print("Все тесты пройдены успешно")
+    print("All tests PASSED")
 
 
 correct_test_caesar()
