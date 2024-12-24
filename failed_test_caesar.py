1ee828a9cf39c8d5a3397da9539bc7194116791b
Elijah Orlov <elijah.n.orlov@yandex.ru>
2024-12-25 00:46:23 +0300

    add workflow

--- failed_test_caesar.py
+++ failed_test_caesar.py
@@ -6,7 +6,7 @@ def failed_test_caesar():
     Не валидная проверка на корректную зашифровку и последующую расшифровку текста со сдвигом 15
     """
     decrypted_text = "ilunination"
-    encrypted_text = "vyhzvangvba" # со сдвигом 13
+    encrypted_text = "vyhzvangvba"  # текст со сдвигом 13
     caesar = Caesar(15)
     check_encrypted_text = caesar.encode(decrypted_text)
     check_decrypted_text = caesar.decode(check_encrypted_text)
